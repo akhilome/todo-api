7e72b28e2b5802fb1f985251d49a3e3a8569a616
Kizito Akhilome <kizito@akhilo.me>
2018-09-17 14:35:48 +0100

    Add user model & setup validators on mongoose models

--- server/server.js
+++ server/server.js
@@ -3,27 +3,25 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
+// Todo Model 
 const Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
-const newTodo = new Todo({
-  text: 'Cook dinner'
-});
-
-// newTodo.save().then(doc => console.log('Saved todo: ', doc)).catch(err => {
-//   console.error('Unable to save todo');
-// });
-
-const secondTodo = new Todo({
+/* const secondTodo = new Todo({
   text: 'Crush this',
   completed: true,
   completedAt: Date.now()
@@ -31,6 +29,24 @@ const secondTodo = new Todo({
 
 secondTodo.save()
   .then(doc => console.log('Saved: ', doc))
-  .catch(err => console.log(err));
+  .catch(err => console.log(err)); */
 
+
+// User Model
+const User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
+});
+
+const firstUser = new User({
+  email: 'kizito@akhilo.me'
+});
+
+firstUser.save()
+  .then(doc => console.log('Saved user: ', doc))
+  .catch(err => console.error('Unable to add user to db', err));
 
