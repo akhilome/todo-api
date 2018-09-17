cb98c1bd6df768a98a7aa7bb9d519858e8b989b1
Kizito Akhilome <kizito@akhilo.me>
2018-09-17 15:14:10 +0100

    Create endpoint to add new todos to db
    - make POST /todos

--- server/server.js
+++ server/server.js
@@ -7,4 +7,16 @@ const { User } = require('../models/user');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  const todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save()
+    .then(doc => res.json(doc))
+    .catch(err => res.status(400).json(err));
+});
+
 app.listen(3000, () => console.log('Running on port 3000'));
