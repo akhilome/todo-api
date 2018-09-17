7ca606e337e8d9bfb4aafd02b290545f82e8fdfa
Kizito Akhilome <kizito@akhilo.me>
2018-09-17 15:01:28 +0100

    Install and configure express

--- server/server.js
+++ server/server.js
@@ -1,4 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const { mongoose } = require('../db/mongoose');
 const { Todo } = require('../models/todo');
 const { User } = require('../models/user');
 
+const app = express();
+
+app.listen(3000, () => console.log('Running on port 3000'));
