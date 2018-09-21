df45a5f433cae7125eac8fe28ee50a240772ca1b
Kizito Akhilome <kizito@akhilo.me>
2018-09-21 17:45:45 +0100

    Write tests for GET /users/me

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -167,3 +167,27 @@ describe('PATCH /todos/:id', () => {
       });
   });
 });
+
+describe('GET /users/me', () => {
+  it('should return user if authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.user._id).toBe(users[0]._id.toHexString());
+        expect(res.body.user.email).toBe(users[0].email);
+      })
+      .end(done);
+  });
+
+  it('should return 401 is not authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect((res) => {
+        expect(res.body).toEqual({});
+      })
+      .end(done);
+  });
+});
